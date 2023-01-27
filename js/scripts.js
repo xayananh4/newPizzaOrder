b835769101ff130504f764505424cd4142ced419
xayananh4 <xayananh4@gmail.com>
2023-01-27 11:59:14 -0800

    add calculateCost()

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
-//BUSINESS
+//BUSINESS LOGIC
 function Pizza(pizzaName, pizzaToppings, pizzaSize) {
   this.name = pizzaName;
   this.toppings= pizzaToppings;
   this.size = pizzaSize;
 }
 
-
-
-
-
-
+Pizza.prototype.calculateCost = function() {
+  
+  let totalPrice = 0;
+  
+  if(this.pizzaName === "MemphisBBQChicken"){
+    totalPrice = 14.99;
+  }
+  else if(this.pizzaName  === "FieryHawaiian"){
+    totalPrice = 17.99;
+  }
+  else {
+    totalPrice = 11.99;
+  }
+  return totalPrice;
+};
 
 // UI Logic
 function getPizza() {
   let selectedPizzaName = document.querySelector('#pizzaOptions').value;
   const selectedSize = document.querySelector("input[name='q01']:checked").value;
   const selectedToppings = document.querySelector("input[name='q02']:checked").value;
+
+  let newPizza = new Pizza(selectedPizzaName,selectedSize,selectedToppings);
+  const totalPrice = newPizza.calculateCost(selectedPizzaName);
+
+  document.getElementById("output").innerHTML = totalPrice;
   
 
 }
