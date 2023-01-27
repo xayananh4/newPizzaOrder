80684ed7832303e2f0becc52b426abd5be854146
xayananh4 <xayananh4@gmail.com>
2023-01-27 12:10:49 -0800

    display final cost to user

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,12 @@ function Pizza(pizzaName, pizzaToppings, pizzaSize) {
 }
 
 Pizza.prototype.calculateCost = function() {
-  
   let totalPrice = 0;
   
-  if(this.pizzaName === "MemphisBBQChicken"){
+  if(this.pizzaName === "Memphis-BBQ-Chicken"){
     totalPrice = 14.99;
   }
-  else if(this.pizzaName  === "FieryHawaiian"){
+  else if(this.pizzaName  === "Fiery-Hawaiian"){
     totalPrice = 17.99;
   }
   else {
@@ -26,25 +25,19 @@ function getPizza() {
   let selectedPizzaName = document.querySelector('#pizzaOptions').value;
   const selectedSize = document.querySelector("input[name='q01']:checked").value;
   const selectedToppings = document.querySelector("input[name='q02']:checked").value;
-
+  
   let newPizza = new Pizza(selectedPizzaName,selectedSize,selectedToppings);
   const totalPrice = newPizza.calculateCost(selectedPizzaName);
-
-  document.getElementById("output").innerHTML = totalPrice;
+  document.getElementById("output").innerHTML = "Total Cost: " + 
+  selectedSize + " " + selectedPizzaName + " With " + selectedToppings + " Extra Toppings: $ " + totalPrice;
   
-
 }
 
-
-
 window.addEventListener("load", function() {
-  
   let form = document.querySelector("form");
 
-    // the original form submission event listener
   form.addEventListener("submit", function(event) {
     getPizza();
-
     event.preventDefault();
   });
 });
